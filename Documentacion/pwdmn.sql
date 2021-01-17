3e04eb2eb81f6db475cf01fb901f1931c64854a4
fernando <jfernando.medinas@alumno.udg.mx>
2021-01-17 11:28:53 -0600

    Subida de la documentacion

---
+++ Documentacion/pwdmn.sql
@@ -0,0 +1,104 @@
+-- MariaDB dump 10.18  Distrib 10.5.8-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: pwdmn
+-- ------------------------------------------------------
+-- Server version	10.5.8-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `correo`
+--
+
+DROP TABLE IF EXISTS `correo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `correo` (
+  `id_correo` int(11) NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_correo`),
+  UNIQUE KEY `direccion` (`direccion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `correo`
+--
+
+LOCK TABLES `correo` WRITE;
+/*!40000 ALTER TABLE `correo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `correo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sesion`
+--
+
+DROP TABLE IF EXISTS `sesion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sesion` (
+  `id_web` int(11) NOT NULL,
+  `id_correo` int(11) NOT NULL,
+  `usuario` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_web`,`id_correo`),
+  KEY `id_correo` (`id_correo`),
+  CONSTRAINT `sesion_ibfk_1` FOREIGN KEY (`id_web`) REFERENCES `web` (`id_web`),
+  CONSTRAINT `sesion_ibfk_2` FOREIGN KEY (`id_correo`) REFERENCES `correo` (`id_correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sesion`
+--
+
+LOCK TABLES `sesion` WRITE;
+/*!40000 ALTER TABLE `sesion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sesion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `web`
+--
+
+DROP TABLE IF EXISTS `web`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `web` (
+  `id_web` int(11) NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alias` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_web`),
+  UNIQUE KEY `direccion` (`direccion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `web`
+--
+
+LOCK TABLES `web` WRITE;
+/*!40000 ALTER TABLE `web` DISABLE KEYS */;
+/*!40000 ALTER TABLE `web` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-17 11:27:20
